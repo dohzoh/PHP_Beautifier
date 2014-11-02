a91482f005f1dbe2458656cfc310238a04390db4
dozo <dohzoh@outlook.com>
2014-11-02 19:29:17 +0900

    add readme

--- scripts/php_beautifier.bat
+++ scripts/php_beautifier.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
+REM SET PHP="F:\local\php\php.exe"
+REM SET BEAUTIFY=.\scripts\\php_beautifier
 SET PHP="@php_bin@"
 SET BEAUTIFY="@bin_dir@"\php_beautifier
 %PHP% -d output_buffering=1 -f %BEAUTIFY% -- %1 %2 %3 %4 %5 %6 %7 %8 %9
